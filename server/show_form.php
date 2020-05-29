d2fc834e7f2f131b5cc0e198ca294496605321dc
AlexCauich <alexcauich12@outlook.com>
2020-05-28 23:40:52 -0500

    helps me display data with ajax

--- server/show_form.php
+++ server/show_form.php
@@ -1,8 +1,27 @@
-<?php 
+<?php
+    include('database.php');
 
-include('database.php');
+    $query = "SELECT * FROM register";
+    $result = mysqli_query($db, $query);
 
-$query = "SELECT * FROM register";
-$result = mysqli_query($db, $query);
+    if(!$result) {
+        die('Query Failed'. mysqli_error($db));
+    }
 
+    $json = array();
+    while($row = mysqli_fetch_array($result)) {
+        $json[] = array(
+            'name_job' => $row['name_job'],
+            'name_service' => $row['name_service'],
+            'phone' => $row['phone'],
+            'place_delivery' => $row['place_delivery'],
+            'anticipo' => $row['anticipo'],
+            'especi' => $row['especi'],
+            'fecha_registro' => $row['fecha_registro'],
+            'id_register' => $row['id_register']
+        );
+    }  
+
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
 ?>
