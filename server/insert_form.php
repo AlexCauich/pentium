a0e15274f506bc2a08dd24d91f745a00c8f8597e
AlexCauich <alexcauich12@outlook.com>
2020-05-28 23:38:14 -0500

    once you have added ajax, the way the data is saved is modified

--- server/insert_form.php
+++ server/insert_form.php
@@ -1,24 +1,21 @@
-<?php
+<?php 
+    include('database.php');
 
-include('database.php');
-
-if(isset($_POST['reg_form'])) {
-    $name_job = $_POST['name_job'];
-    $name_service = $_POST['name_service'];
-    $phone = $_POST['phone'];
-    $place_delivery = $_POST['place_delivery'];
-    $anticipo = $_POST['anticipo'];
-    $especi = $_POST['especi'];
-
-    $date = date('Y-m-d');
-
-    $query = "INSERT INTO register(name_job,name_service,phone,place_delivery,anticipo,especi,fecha_registro) VALUES('$name_job', '$name_service', '$phone', '$place_delivery', '$anticipo', '$especi', '$date')";
-    $result = mysqli_query($db, $query);
-    if(!$result) {
-        die('Query Failed');
+    if(isset($_POST['name_job'])) {
+        $name_job = $_POST['name_job'];
+        $name_service = $_POST['name_service'];
+        $phone = $_POST['phone'];
+        $place_delivery = $_POST['place_delivery'];
+        $anticipo = $_POST['anticipo'];
+        $especi = $_POST['especi'];
+    
+        $date = date('Y-m-d');
+    
+        $query = "INSERT INTO register(name_job,name_service,phone,place_delivery,anticipo,especi,fecha_registro) VALUES('$name_job', '$name_service', '$phone', '$place_delivery', '$anticipo', '$especi', '$date')";
+        $result = mysqli_query($db, $query);
+        if(!$result) {
+            die('Query Failed.');
+        }
+        echo 'Register Added Successfully';
     }
-
-    header('location: ../index.php');
-}
-
 ?>
